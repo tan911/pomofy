6b73aba555468c6d5a3bda0630b028d75af75ec0
tan <jolanutan@gmail.com>
2024-04-08 17:07:52 +0800

    test

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -15,6 +15,7 @@ export const validateRequest = cache(
         }
 
         const result = await lucia.validateSession(sessionId)
+        // next.js throws when you attempt to set cookie when rendering page
         try {
             if (result.session && result.session.fresh) {
                 const sessionCookie = lucia.createSessionCookie(result.session.id)
