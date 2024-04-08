4cef8d674a2d235c7203ee742989c7d6e7187e6b
tan <jolanutan@gmail.com>
2024-04-08 16:36:17 +0800

    test workflow deployment

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function PageNotFound() {
+    return (
+        <main className="flex h-screen items-center justify-center text-center">
+            <div>
+                <h1>404: Not Found</h1>
+                <p>The page you requested could not be found.</p>
+            </div>
+        </main>
+    )
+}
