576771ebc44313b2c524bf8561ebd28f91cac719
tan <jolanutan@gmail.com>
2024-04-10 21:49:15 +0800

    fix server deployment

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -9,7 +9,7 @@ process.on('uncaughtException', (error) => {
     logger.error(`Uncaught Exception: ${error}`)
 })
 
-const server = app.listen(process.env.SERVER_PORT, () => {
+const server = app.listen(process.env.SERVER_PORT ?? 5000, () => {
     logger.info(`Server running at \thttp://localhost:${process.env.SERVER_PORT ?? 5000}`)
 })
 
